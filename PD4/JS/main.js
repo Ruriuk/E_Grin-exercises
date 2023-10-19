1f25fbf755d7c58908804993b01b577a1899c847
Ernest <grin.ernest2005@gmail.com>
2023-10-19 14:19:06 +0300

    PD4 solution

---
+++ PD4/JS/main.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "Kepsnis";
+petAge = 15;
+
+console.log("Pet's name: " + petName);
+console.log("Pet's age: " + petAge);
+
+petName = "Brigada";
+petAge = 9;
+
+const buildingAddress = "Ozo g. 21.";
+
+console.log("Building Address: " + buildingAddress);
