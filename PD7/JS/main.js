84cb67bebb804907de4152acfc7118cbe1a5b2de
Ernest <grin.ernest2005@gmail.com>
2023-11-09 09:12:47 +0200

    PD7 solution

---
+++ PD7/JS/main.js
@@ -0,0 +1,20 @@
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne + numberTwo);
+console.log(numberOne - numberTwo);
+console.log(numberOne * numberTwo);
+console.log(numberOne / numberTwo); 
+console.log(numberOne % numberTwo);
+console.log(numberOne ** numberTwo); 
+
+console.log(numberOne++); 
+console.log(++numberOne);
+
+console.log(numberOne--);
+console.log(--numberOne);
+
+console.log(--numberOne + numberTwo++);
+console.log(numberOne-- - --numberTwo + numberTwo++);
+console.log(numberOne++ / numberTwo++); 
+console.log(numberOne++ * --numberTwo); 
