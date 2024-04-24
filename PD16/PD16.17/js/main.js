62f621000115781fcc9809ba5b576f6fa0526e87
Ernest <grin.ernest2005@gmail.com>
2024-04-24 11:57:06 +0300

    PD16.17 solution

--- PD16/PD16.17/js/main.js
+++ PD16/PD16.17/js/main.js
@@ -1,16 +1,24 @@
-function trikampioPlotas(a, b, c) {
-
-    let s = (a + b + c) / 2;
-    
-    let plotas = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    
-    return plotas;
+function arTobulas(sk) {
+    if (sk < 1) {
+      return false; // Neigiami skaičiai ir nulis nėra tobulieji
+    }
+  
+    var sumaDalikliu = 0;
+  
+    for (var daliklis = 1; daliklis <= sk / 2; daliklis++) {
+      if (sk % daliklis === 0) {
+        sumaDalikliu += daliklis;
+      }
+    }
+  
+    return sumaDalikliu === sk;
   }
   
+  var skaicius = 28;
   
-  let A = 5;
-  let B = 6;
-  let C = 7;
+  if (arTobulas(skaicius)) {
+    console.log(skaicius + " yra tobulasis skaičius.");
+  } else {
+    console.log(skaicius + " nėra tobulasis skaičius.");
+  }
   
-  let plotas = trikampioPlotas(A, B, C);
-  console.log("Plotas: ", plotas);
