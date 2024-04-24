f464f79c6c8bdb2c6f6fe9ae81a75d8fefb65892
Ernest <grin.ernest2005@gmail.com>
2024-04-24 11:58:26 +0300

    PD16.17 solution

--- PD16/PD16.17/js/main.js
+++ PD16/PD16.17/js/main.js
@@ -1,24 +1,14 @@
-function arTobulas(sk) {
-    if (sk < 1) {
-      return false; // Neigiami skaičiai ir nulis nėra tobulieji
-    }
-  
-    var sumaDalikliu = 0;
-  
-    for (var daliklis = 1; daliklis <= sk / 2; daliklis++) {
-      if (sk % daliklis === 0) {
-        sumaDalikliu += daliklis;
-      }
-    }
-  
-    return sumaDalikliu === sk;
+function didesniUzNurodyta(skaicius, masyvas) {
+    return masyvas.filter(function(elementas) {
+      return elementas > skaicius;
+    });
   }
   
-  var skaicius = 28;
+  var skaiciusNurodytas = 5;
+  var manoMasyvas = [2, 8, 3, 10, 5, 7];
   
-  if (arTobulas(skaicius)) {
-    console.log(skaicius + " yra tobulasis skaičius.");
-  } else {
-    console.log(skaicius + " nėra tobulasis skaičius.");
-  }
+  var rezultatas = didesniUzNurodyta(skaiciusNurodytas, manoMasyvas);
+  
+  console.log("Pradinis masyvas:", manoMasyvas);
+  console.log("Elementai didesni už", skaiciusNurodytas + ":", rezultatas);
   
