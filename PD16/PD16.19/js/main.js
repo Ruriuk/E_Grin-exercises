711610363337999b3bebda6760784884d3466788
Ernest <grin.ernest2005@gmail.com>
2024-04-24 12:00:40 +0300

    PD16.18 solution

--- PD16/PD16.19/js/main.js
+++ PD16/PD16.19/js/main.js
@@ -1,16 +1,22 @@
-function trikampioPlotas(a, b, c) {
-
-    let s = (a + b + c) / 2;
+function bubbleSort(masyvas) {
+    var ilgis = masyvas.length;
     
-    let plotas = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    
-    return plotas;
+    for (var i = 0; i < ilgis - 1; i++) {
+      for (var j = 0; j < ilgis - i - 1; j++) {
+        if (masyvas[j] > masyvas[j + 1]) {
+          var temp = masyvas[j];
+          masyvas[j] = masyvas[j + 1];
+          masyvas[j + 1] = temp;
+        }
+      }
+    }
+  
+    return masyvas;
   }
   
+  var pradinisMasyvas = [64, 34, 25, 12, 22, 11, 90];
+  var surusiuotasMasyvas = bubbleSort(pradinisMasyvas);
   
-  let A = 5;
-  let B = 6;
-  let C = 7;
+  console.log("Pradinis masyvas:", pradinisMasyvas);
+  console.log("Surūšiuotas masyvas:", surusiuotasMasyvas);
   
-  let plotas = trikampioPlotas(A, B, C);
-  console.log("Plotas: ", plotas);
