c3d8d6379c87037555686d6ae3f65345718fed8a
Ernest <grin.ernest2005@gmail.com>
2024-04-25 19:54:33 +0300

    PD18.3 solution

---
+++ PD18/PD18.3/js/main.js
@@ -0,0 +1,13 @@
+const knyga = {
+    pavadinimas: 'Grybu musis',
+    autorius: 'John Smith',
+    puslapiuSkaicius: 170
+};
+
+function knygosInfo(knyga) {
+    console.log('Knygos pavadinimas:', knyga.pavadinimas);
+    console.log('Autorius:', knyga.autorius);
+    console.log('Puslapiu skaicius:', knyga.puslapiuSkaicius);
+}
+
+knygosInfo(knyga);
